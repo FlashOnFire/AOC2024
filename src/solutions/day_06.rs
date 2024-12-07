3f20c9dd91fca5c6e3b58328239b8d01a805c1fa
FlashOnFire_ <flashonfire.dev@gmail.com>
2024-12-07 21:44:24 +0100

    36ms

--- src/solutions/day_06.rs
+++ src/solutions/day_06.rs
@@ -1,5 +1,5 @@
 use crate::common::{Answer, Solution};
-use fxhash::FxHashSet;
+use fxhash::{hash, FxHashSet};
 use itertools::Itertools;
 use rayon::iter::ParallelIterator;
 use rayon::prelude::ParallelBridge;
@@ -15,6 +15,7 @@ enum Facing {
 }
 
 impl Facing {
+    #[inline(always)]
     fn turn_90_degrees(&mut self) {
         *self = match self {
             Facing::Up => Facing::Right,
@@ -46,6 +47,7 @@ fn advance(
     }
 }
 
+#[inline(always)]
 fn advance2(
     obstacles: &FxHashSet<(i32, i32)>,
     guard: &mut (i32, i32),
@@ -67,6 +69,7 @@ fn advance2(
     }
 }
 
+#[inline(always)]
 fn compute_path(input: &str) -> Vec<(i32, i32)> {
     let mut obstacles = vec![];
     let mut guard: (i32, i32) = (-1, -1);
@@ -84,7 +87,7 @@ fn compute_path(input: &str) -> Vec<(i32, i32)> {
     assert_ne!(guard, (-1, -1));
 
     let size_y = input.lines().count();
-    let size_x = input.lines().nth(0).unwrap().len();
+    let size_x = input.lines().next().unwrap().len();
     let mut path = vec![];
 
     while guard.0 >= 0 && guard.1 >= 0 && guard.0 < size_x as i32 && guard.1 < size_y as i32 {
@@ -117,14 +120,18 @@ impl Solution for Day06 {
                 }
             }
         }
-        assert_ne!(default_guard, (-1, -1));
+        debug_assert_ne!(default_guard, (-1, -1));
 
         let size_y = input.lines().count() as i32;
         let size_x = input.lines().nth(0).unwrap().len() as i32;
 
         let mut base_path = vec![];
+
         let mut guard = default_guard;
         let mut facing = Facing::Up;
+
+        //advance first to make sure starting point is not in base path
+        advance2(&obstacles, &mut guard, &mut facing, Some((0, 0)));
         while guard.0 >= 0 && guard.1 >= 0 && guard.0 < size_x && guard.1 < size_y {
             base_path.push(guard);
             advance2(&obstacles, &mut guard, &mut facing, None);
@@ -132,9 +139,8 @@ impl Solution for Day06 {
 
         base_path
             .iter()
-            .unique()
+            .unique_by(hash)
             .par_bridge()
-            .filter(|pos| !obstacles.contains(pos) && **pos != default_guard)
             .map(|pos| {
                 let mut guard = default_guard;
                 let mut facing = Facing::Up;
@@ -146,20 +152,38 @@ impl Solution for Day06 {
                     FxHashSet::default(),
                 );
 
-                while guard.0 >= 0 && guard.1 >= 0 && guard.0 < size_x && guard.1 < size_y {
+                loop {
                     if !match &facing {
-                        Facing::Up => path.0.insert(guard),
-                        Facing::Down => path.1.insert(guard),
-                        Facing::Left => path.2.insert(guard),
-                        Facing::Right => path.3.insert(guard),
+                        Facing::Up => {
+                            if guard.1 < 0 {
+                                return 0;
+                            }
+                            path.0.insert(guard)
+                        }
+                        Facing::Down => {
+                            if guard.1 >= size_y {
+                                return 0;
+                            }
+                            path.1.insert(guard)
+                        }
+                        Facing::Left => {
+                            if guard.0 < 0 {
+                                return 0;
+                            }
+                            path.2.insert(guard)
+                        }
+                        Facing::Right => {
+                            if guard.0 >= size_x {
+                                return 0;
+                            }
+                            path.3.insert(guard)
+                        }
                     } {
                         return 1;
                     }
 
                     advance2(&obstacles, &mut guard, &mut facing, Some(*pos));
                 }
-
-                0
             })
             .sum::<i32>()
             .into()
